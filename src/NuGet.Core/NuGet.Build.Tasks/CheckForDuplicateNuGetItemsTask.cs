3cec28d175431df712acf6405989319a8a92bf84
Nikolche Kolev <nikolev@microsoft.com>
2021-04-06 10:30:42 -0700

    fix formatting

--- src/NuGet.Core/NuGet.Build.Tasks/CheckForDuplicateNuGetItemsTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/CheckForDuplicateNuGetItemsTask.cs
@@ -1,16 +1,16 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-using NuGet.Commands;
-using NuGet.Common;
-using NuGet.ProjectModel;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Commands;
+using NuGet.Common;
+using NuGet.ProjectModel;
 
 
 namespace NuGet.Build.Tasks
