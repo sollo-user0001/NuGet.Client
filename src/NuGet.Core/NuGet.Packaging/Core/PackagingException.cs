fb37b58382d59cdfb6a5c48dbca2a309647bac40
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-09-29 16:50:43 -0700

    [skip ci] ca2237 example

--- src/NuGet.Core/NuGet.Packaging/Core/PackagingException.cs
+++ src/NuGet.Core/NuGet.Packaging/Core/PackagingException.cs
@@ -2,10 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Runtime.Serialization;
 using NuGet.Common;
 
 namespace NuGet.Packaging.Core
 {
+    [Serializable]
     public class PackagingException : Exception, ILogMessageException
     {
         private readonly IPackLogMessage _logMessage;
@@ -37,6 +39,21 @@ namespace NuGet.Packaging.Core
         public virtual ILogMessage AsLogMessage()
         {
             return _logMessage;
+        }
+
+        protected PackagingException(SerializationInfo serializationInfo, StreamingContext streamingContext) :
+            base(serializationInfo, streamingContext)
+        {
+            string logCode = serializationInfo.GetString("logCode");
+            string message = serializationInfo.GetString("Message");
+
+            _logMessage = PackagingLogMessage.CreateError(message, (NuGetLogCode)Enum.Parse(typeof(NuGetLogCode), logCode));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("logCode", _logMessage.Code.ToString());
         }
     }
 }
